653e05df1353cdffddffb9100b72a2a996f538f7
Gopal4444t <gopalthombre921@gmail.com>
2026-09-22 11:15:19 +0530

    Create Advance_Python_Programming.py

---
+++ Advance_Python_Programming.py
@@ -0,0 +1,80 @@
+def my_decorator(func):
+    def wrapper():
+        print("Before")
+        func()
+        print("After")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+say_hello()
+
+# class decorator
+def fun(cls):
+    cls.class_name = cls.__name__
+    return cls
+
+@fun
+class Person:
+    pass
+
+print(Person.class_name)
+
+
+# closure
+def outer_function(x):
+    def inner_function(y):
+        return x + y
+    return inner_function
+
+closure = outer_function(10)
+print(closure(5))
+print(closure(20))
+
+# custom iterator
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+    print(x)
+
+# decorator with parameter 
+def decorator_name(func):
+    def wrapper(*args, **kwargs):
+        print("Before execution")
+        result = func(*args, **kwargs)
+        print("After execution")
+        return result
+    return wrapper
+
+@decorator_name
+def add(a, b):
+    return a + b
+
+print(add(5, 2))
+
+
+# generator 
+def fun(max):
+    cnt = 1
+    while cnt <= max:
+        yield cnt
+        cnt += 1
+
+ctr = fun(5)
+for n in ctr:
+    print(n)
